1676fad7f5303c564a817390266ab4c04a54f54a
Nassima-code <nassima.eljazouli@etu.uae.ac.ma>
2022-02-04 01:36:05 +0100

    First commit

---
+++ src/main/java/org/lsi/AuthentificationApplication.java
@@ -0,0 +1,19 @@
+package org.lsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class AuthentificationApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/lsi/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package org.lsi.repositories;
+
+import org.lsi.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+}
