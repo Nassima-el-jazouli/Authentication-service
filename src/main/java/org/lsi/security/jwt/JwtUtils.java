5993567ca63c238c321a1ab2eeafb053272974cb
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-03-20 19:02:16 +0100

    Second push

--- src/main/java/org/lsi/security/jwt/JwtUtils.java
+++ src/main/java/org/lsi/security/jwt/JwtUtils.java
@@ -15,10 +15,10 @@ import io.jsonwebtoken.*;
 public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-	@Value("${bezkoder.app.jwtSecret}")
+	@Value("${nassima.app.jwtSecret}")
 	private String jwtSecret;
 
-	@Value("${bezkoder.app.jwtExpirationMs}")
+	@Value("${nassima.app.jwtExpirationMs}")
 	private int jwtExpirationMs;
 
 	public String generateJwtToken(Authentication authentication) {
